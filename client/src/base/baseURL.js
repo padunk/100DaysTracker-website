b3ab28a1d603a311d6f38e92f49f6a9592860040
Padunk <abraham.agel@gmail.com>
2020-04-15 18:40:46 +0700

    change port

--- client/src/base/baseURL.js
+++ client/src/base/baseURL.js
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5000/api";
+  process.env.NODE_ENV === "production"
+    ? process.env.PORT + "/api"
+    : "http://localhost:5000/api";
