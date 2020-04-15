61cf80000896df8e3d55735bec9842252f6cd2fa
Padunk <abraham.agel@gmail.com>
2020-04-15 11:25:43 +0700

    create db instruction

---
+++ database/createDatabase.sql
@@ -0,0 +1,32 @@
+-- CREATE DATABASE FIRST IN YOUR LOCAL POSTGRES DB
+-- YOU DON'T NEET TO DO THIS WITH HEROKU POSTGRES ADDONS
+CREATE DATABASE challenges;
+
+-- create table challenge
+CREATE TABLE challenge
+(
+    challenge_id TEXT PRIMARY KEY NOT NULL,
+    title VARCHAR(20) UNIQUE,
+    hash_tag VARCHAR(30),
+    goal VARCHAR(30),
+    date_created INTEGER
+);
+
+-- create table for challenge_detail
+CREATE TABLE challenge_detail
+(
+    parent_id TEXT REFERENCES challenge(challenge_id),
+    tweet VARCHAR(140),
+    date_created INTEGER
+);
+
+--  create table for skill_list
+CREATE TABLE skill_list
+(
+    skill_id TEXT,
+    skill_name VARCHAR(50) NOT NULL UNIQUE,
+    new_skill INTEGER,
+    progress_skill INTEGER,
+    complete_skill INTEGER,
+    date_created INTEGER
+);
