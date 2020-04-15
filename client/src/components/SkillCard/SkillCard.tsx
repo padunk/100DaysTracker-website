ade4a77be6a7d59a16eb84cd2cc46eb18d0e868c
Padunk <abraham.agel@gmail.com>
2020-04-15 16:27:44 +0700

    adding flip

--- client/src/components/SkillCard/SkillCard.tsx
+++ client/src/components/SkillCard/SkillCard.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 import classNames from "classnames";
+import { Flipper, Flipped } from "react-flip-toolkit";
 
 import Wrapper from "../Wrapper/Wrapper";
 import SvgShuttle from "../../icons/Shuttle";
@@ -31,71 +32,75 @@ function SkillDetail({ title, skills, id, handleClick }: Props): ReactElement {
       <Wrapper customClass="bg-gray-800 text-white h-10 flex items-center px-4 border border-gray-100">
         <h3>{title}</h3>
       </Wrapper>
-      <ul>
-        {skills.map((skill: ISkills, idx: number) => {
-          const listClassNames = classNames(
-            "flex flex-wrap justify-between items-center py-2 px-4",
-            { "bg-gray-400": idx % 2 !== 0, "bg-gray-100": idx % 2 === 0 }
-          );
-          return (
-            <li key={skill.skill_id} className={listClassNames}>
-              <span className="text-lg">{skill.skill_name}</span>
-              <div className="flex flex-wrap">
-                {id === 1 && (
-                  <IconButton type="button">
-                    <SvgShuttle
-                      id={skill.skill_id}
-                      fill="#3182ce"
-                      className='w-4 h-4'
-                      onClick={handleClick}
-                    />
-                  </IconButton>
-                )}
-                {id === 2 && (
-                  <>
-                    <IconButton type="button">
-                      <SvgCheckmark
-                        id={skill.skill_id}
-                        fill="#48bb78"
-                        className='w-4 h-4'
-                        onClick={handleClick}
-                      />
-                    </IconButton>
+      <Flipper flipKey={skills.join("")}>
+        <ul>
+          {skills.map((skill: ISkills, idx: number) => {
+            const listClassNames = classNames(
+              "flex flex-wrap justify-between items-center py-2 px-4",
+              { "bg-gray-400": idx % 2 !== 0, "bg-gray-100": idx % 2 === 0 }
+            );
+            return (
+              <Flipped key={skill.skill_id} flipId={skill.skill_id}>
+                <li className={listClassNames}>
+                  <span className="text-lg">{skill.skill_name}</span>
+                  <div className="flex flex-wrap">
+                    {id === 1 && (
+                      <IconButton type="button">
+                        <SvgShuttle
+                          id={skill.skill_id}
+                          fill="#3182ce"
+                          className="w-4 h-4"
+                          onClick={handleClick}
+                        />
+                      </IconButton>
+                    )}
+                    {id === 2 && (
+                      <>
+                        <IconButton type="button">
+                          <SvgCheckmark
+                            id={skill.skill_id}
+                            fill="#48bb78"
+                            className="w-4 h-4"
+                            onClick={handleClick}
+                          />
+                        </IconButton>
+                        <Gap className="w-4" />
+                        <IconButton type="button">
+                          <SvgUpdateArrow
+                            id={skill.skill_id}
+                            fill="#9f7aea"
+                            className="w-4 h-4"
+                            onClick={handleClick}
+                          />
+                        </IconButton>
+                      </>
+                    )}
+                    {id === 3 && (
+                      <IconButton type="button">
+                        <SvgUpdateArrow
+                          id={skill.skill_id}
+                          fill="#9f7aea"
+                          className="w-4 h-4"
+                          onClick={handleClick}
+                        />
+                      </IconButton>
+                    )}
                     <Gap className="w-4" />
                     <IconButton type="button">
-                      <SvgUpdateArrow
+                      <SvgDelete
                         id={skill.skill_id}
-                        fill="#9f7aea"
-                        className='w-4 h-4'
+                        fill="#e53e3e"
+                        className="w-4 h-4"
                         onClick={handleClick}
                       />
                     </IconButton>
-                  </>
-                )}
-                {id === 3 && (
-                  <IconButton type="button">
-                    <SvgUpdateArrow
-                      id={skill.skill_id}
-                      fill="#9f7aea"
-                      className='w-4 h-4'
-                      onClick={handleClick}
-                    />
-                  </IconButton>
-                )}
-                <Gap className="w-4" />
-                <IconButton type="button">
-                  <SvgDelete
-                    id={skill.skill_id}
-                    fill="#e53e3e"
-                    className='w-4 h-4'
-                    onClick={handleClick}
-                  />
-                </IconButton>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+                  </div>
+                </li>
+              </Flipped>
+            );
+          })}
+        </ul>
+      </Flipper>
     </Wrapper>
   );
 }
